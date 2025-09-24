31012980f3519db934c69fc5cfdcf22c2198984c
vz235711 <vz235711@gmail.com>
2025-09-24 11:12:16 +0700

    first commit

---
+++ app.py
@@ -0,0 +1,10 @@
+import json
+import math
+from typing import Dict, Tuple, List
+
+import numpy as np
+import pandas as pd
+import requests
+import streamlit as st
+
+st.set_page_config(page_title="FPL FDR (Custom Weights)", layout="wide")
