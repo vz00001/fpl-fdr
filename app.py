81f15fc94d90071cff633254c61c8e4a9491c784
vz235711 <vz235711@gmail.com>
2025-10-30 17:46:12 +0700

    ICT index 3

--- app.py
+++ app.py
@@ -151,11 +151,11 @@ with st.sidebar:
             st.success("Preset loaded.")
         except Exception as e:
             st.error(f"Invalid preset: {e}")
-    # # --- Sidebar: Team visibility ---
-    # st.subheader("Teams")
+    # --- Sidebar: Team visibility ---
+    st.subheader("Teams")
 
-    # _all = teams_df.sort_values("name")
-    # id_options = list(map(int, _all["team_id"]))
+    _all = teams_df.sort_values("name")
+    id_options = list(map(int, _all["team_id"]))
 
     # def _fmt_team(tid: int) -> str:
     #     r = _all.loc[_all["team_id"] == tid].iloc[0]
@@ -167,9 +167,9 @@ with st.sidebar:
     #     default=id_options,           # all selected by default
     #     format_func=_fmt_team,
     # )
-    # # safety: if user deselects everything, fallback to all
-    # if not visible_ids:
-    #     visible_ids = id_options
+    # safety: if user deselects everything, fallback to all
+    if not visible_ids:
+        visible_ids = id_options
 
 # ---------- Build & display ticker ----------
 disp_df, val_df = core.build_ticker(
