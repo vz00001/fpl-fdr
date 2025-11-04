90b2eaff8872fc8372fd71215472cf6a47ae74f1
vz235711 <vz235711@gmail.com>
2025-11-04 15:29:38 +0700

    ICT recent gw

--- fpl_core.py
+++ fpl_core.py
@@ -353,7 +353,7 @@ def apply_rolling(players_df_slice: pd.DataFrame, n: int) -> pd.DataFrame:
 
     return joined.sort_values("ict_index", ascending=False, kind="mergesort").reset_index(drop=True)
 
-# fpl-fdr/quick-test.py
-teams_df, fixtures_df, event_df, fetched_at, players_df = fetch_fpl_data()
-# print(fetch_player_history(1))
-print(apply_rolling(combine_filters("DF", 7, players_df), 5).head(20))
+# # fpl-fdr/quick-test.py
+# teams_df, fixtures_df, event_df, fetched_at, players_df = fetch_fpl_data()
+# # print(fetch_player_history(1))
+# print(apply_rolling(combine_filters("DF", 7, players_df), 5).head(20))
